c0d26e9b2622813a3ce017ec6e3da38ed157cf49
aninaja <alyiannejanina.cava@gmail.com>
2023-10-31 21:53:26 +0800

    Fixed loyalty_app

--- loyalty_app/views.py
+++ loyalty_app/views.py
@@ -1,8 +1,14 @@
+from datetime import datetime
+
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+
+from user_profile.models import UserProfile
+
+User = get_user_model()
 
 
 # Create your views here.
@@ -12,16 +18,24 @@ def home(request):
 
 
 def process_login(request):
+
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
 
+        date_now = datetime.now()
+
         if user is not None:
-            if user.is_patient and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect('/home')
+            if user.is_patient:
+                if date_now > user.userprofile.account_expiry:
+                    messages.error(request, "Your account is already expired. Please renew your account.")
+                    return render(request, 'customer_base_login.html')
+
+                else:
+                    login(request, user)
+                    return HttpResponseRedirect('/home')
 
             else:
                 messages.error(request, "Your username and password didn't match. Please try again.")
