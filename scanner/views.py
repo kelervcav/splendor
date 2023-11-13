c849c87b6007e0c6bb36400694b59d40130d9626
aninaja <alyiannejanina.cava@gmail.com>
2023-11-14 02:16:40 +0800

    Modified initial commit

--- scanner/views.py
+++ scanner/views.py
@@ -1,29 +1,29 @@
-from django.shortcuts import render
-from pyzbar.pyzbar import decode
-import cv2
-
-
-# Create your views here.
-# def index(request):
-#     return render(request, 'scanner.html')
+# from django.shortcuts import render
+# from pyzbar.pyzbar import decode
+# import cv2
 #
-
-def scan_qrcode(request):
-    cam = cv2.VideoCapture(0)
-    cam.set(5, 640)
-    cam.set(5, 480)
-
-    camera = True
-    while camera:
-        success, frame = cam.read()
-
-        for i in decode(frame):
-            print(i.type)
-            print(i.data.decode('utf-8'))
-
-            cv2.imshow('QR Code Scanner', frame)
-            cv2.waitKey(3)
-
-        cam.release()
-
-    return render(request, 'scanner.html')
+#
+# # Create your views here.
+# # def index(request):
+# #     return render(request, 'scanner.html')
+# #
+#
+# def scan_qrcode(request):
+#     cam = cv2.VideoCapture(0)
+#     cam.set(5, 640)
+#     cam.set(5, 480)
+#
+#     camera = True
+#     while camera:
+#         success, frame = cam.read()
+#
+#         for i in decode(frame):
+#             print(i.type)
+#             print(i.data.decode('utf-8'))
+#
+#             cv2.imshow('QR Code Scanner', frame)
+#             cv2.waitKey(3)
+#
+#         cam.release()
+#
+#     return render(request, 'scanner.html')
